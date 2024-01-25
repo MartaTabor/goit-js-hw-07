8c9fd2c744bdd7b1af8bbba6f8eba228c0407ff6
Marta Tabor <leeloothefifth@googlemail.com>
2024-01-25 20:18:36 +0000

    gallery-add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,13 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+const galleryArray = galleryItems.map(
+  (item) =>
+    `<li class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/></a></li>`
+);
+
+gallery.innerHTML = galleryArray.join("");
 
 console.log(galleryItems);
